0069c1b68e65ca146e4732693a69c16b9eed6aaa
nakakonakako <kotamina2016@gmail.com>
2025-09-08 20:31:23 +0900

    merge main

--- main.cpp
+++ main.cpp
@@ -13,10 +13,27 @@ int calc(int N, int L, vector<int> A) {
     return cnt;
 }
 
+<<<<<<< Updated upstream
 output() {}
 
 main() {
     input();
     m = calc(M);
     output();
+=======
+int calc(int M) {
+    return 48 - M;
+}
+
+void output(int m) {
+    cout << m << endl;
+}
+
+int main() {
+    int m;
+    m = input();
+    m = calc(m);
+    output(m);
+    return 0;
+>>>>>>> Stashed changes
 }
